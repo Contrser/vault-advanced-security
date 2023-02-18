f81ab133c80634d8591771d1ec64c8073b2ce6f6
Contrser <victoria1209higo@gmail.com>
2023-02-18 14:00:00 +0200

    Refactor storage layout for better efficiency

--- escrow.go
+++ escrow.go
@@ -1,3 +1,4 @@
 // Fix vulnerability in decentralized finance protocols
 // Improve cross-chain asset management
 // Optimize storage for off-chain data
+// Add support for fractional token ownership
