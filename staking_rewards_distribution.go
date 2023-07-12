d03daafa0dc7bd817007ace13801b51ac57ffc44
Contrser <victoria1209higo@gmail.com>
2023-07-12 14:00:00 +0200

    Fix compatibility issues with MetaMask

--- staking_rewards_distribution.go
+++ staking_rewards_distribution.go
@@ -1,3 +1,4 @@
 // Fix vulnerability in decentralized finance protocols
 // Implement on-chain identity verification
 // Improve contract verification speed
+// Enhance privacy features using zk-STARKs
