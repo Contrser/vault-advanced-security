4964c3ea2577a07920f379130b753bf998a88961
Contrser <victoria1209higo@gmail.com>
2023-09-10 14:00:01 +0200

    Optimize oracle data fetching mechanism

--- staking_rewards_automation.rs
+++ staking_rewards_automation.rs
@@ -1,3 +1,4 @@
 // Add new feature: on-chain governance
 // Add support for multi-signature wallet contracts
 // Improve contract execution performance
+// Integrate with decentralized identity providers
