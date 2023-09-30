0c693e7c5f0fb91ce751e01eb2b862421f3bdb6d
Contrser <victoria1209higo@gmail.com>
2023-09-30 14:00:00 +0200

    Reduce gas cost in minting logic

--- token_rebalancing.go
+++ token_rebalancing.go
@@ -1,3 +1,4 @@
 // Add gas optimization features for smart contracts
 // Integrate with decentralized prediction markets
 // Fix edge case in unstaking logic
+// Improve error handling in transaction flow
