225181e92038ade69d523efa27e5287e5897e9f7
Contrser <victoria1209higo@gmail.com>
2023-11-16 14:00:00 +0200

    Improve decentralized identity (DID) systems

--- transaction_processing.go
+++ transaction_processing.go
@@ -1,3 +1,4 @@
 // Add support for decentralized file storage
 // Add support for decentralized credit scoring
 // Integrate with external DeFi protocols
+// Enhance privacy features using zk-STARKs
