70a63a6f2b47873b894ff36458b4b521218ebc3e
Contrser <victoria1209higo@gmail.com>
2023-11-20 14:00:00 +0200

    Add governance proposal expiration feature

--- contract_initialization.sol
+++ contract_initialization.sol
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Improve decentralized lending contract flexibility
 // Optimize decentralized voting mechanisms
+// Fix overflow bug in token calculation
