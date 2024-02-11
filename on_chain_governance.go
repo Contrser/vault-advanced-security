bbe28b4f105f0521d3cf8807747cf08ec9374da4
Contrser <victoria1209higo@gmail.com>
2024-02-11 14:00:02 +0200

    Improve ERC-721 token metadata functionality

--- on_chain_governance.go
+++ on_chain_governance.go
@@ -1,3 +1,4 @@
 // Refactor incentive structure in DeFi protocol
 // Implement automated governance voting
 // Add governance proposal expiration feature
+// Fix ERC-721 contract issues with transfers
