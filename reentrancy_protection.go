02ede64a2cc62e1260e58bfbbd61a0cd9de7055b
Contrser <victoria1209higo@gmail.com>
2024-04-12 14:00:02 +0200

    Add new RPC endpoint

--- reentrancy_protection.go
+++ reentrancy_protection.go
@@ -1,3 +1,4 @@
 // Improve error handling in transaction flow
 // Fix overflow bug in token calculation
 // Add governance proposal expiration feature
+// Add support for multi-signature wallet contracts
