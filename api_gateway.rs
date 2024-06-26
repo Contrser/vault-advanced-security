e9b8a2c88a2126fad4fe1a33f1e75c8e435bcb41
Contrser <victoria1209higo@gmail.com>
2024-06-26 14:00:00 +0200

    Fix issues with token vesting contracts

--- api_gateway.rs
+++ api_gateway.rs
@@ -1,3 +1,4 @@
 // Implement liquidity pool staking mechanism
 // Fix security vulnerability in random number generation
 // Enhance smart contract upgradeability
+// Enhance governance contract flexibility
