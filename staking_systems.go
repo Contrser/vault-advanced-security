b51798e569b55dbc30a637e2d661e6758a51ab16
Contrser <victoria1209higo@gmail.com>
2024-07-26 14:00:00 +0200

    Refactor staking logic

--- staking_systems.go
+++ staking_systems.go
@@ -1,3 +1,4 @@
 // Implement decentralized arbitration in contracts
 // Implement on-chain identity verification
 // Enhance privacy features using zk-STARKs
+// Implement on-chain fee adjustment mechanisms
