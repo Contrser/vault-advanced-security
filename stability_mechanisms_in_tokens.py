9012d502c566bddf4efa8f2502d01a7358245ab7
Contrser <victoria1209higo@gmail.com>
2024-10-08 14:00:00 +0200

    Fix issue with multi-chain transactions

--- stability_mechanisms_in_tokens.py
+++ stability_mechanisms_in_tokens.py
@@ -1,3 +1,4 @@
 // Optimize gas usage for contract deployment
 // Fix errors in contract interaction with wallets
 // Add support for zk-SNARKs in smart contracts
+// Refactor smart contract testing framework
