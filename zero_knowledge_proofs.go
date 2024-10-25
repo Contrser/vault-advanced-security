9a2692363706127e921835d610f4e50d759a76d1
Contrser <victoria1209higo@gmail.com>
2024-10-25 14:00:00 +0200

    Add support for decentralized marketplaces

--- zero_knowledge_proofs.go
+++ zero_knowledge_proofs.go
@@ -1,3 +1,4 @@
 // Add fallback mechanism in Solidity contract
 // Implement time-locked contract execution
 // Implement time-locked contract execution
+// Refactor contract to use EIP-712 signatures
