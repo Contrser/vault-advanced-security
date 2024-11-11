56c35eb29f1fc9d8ddab3b7d5d1c7b588987844d
Contrser <victoria1209higo@gmail.com>
2024-11-11 14:00:00 +0200

    Integrate automated market maker (AMM) logic

--- staking_pool_management.rs
+++ staking_pool_management.rs
@@ -5,3 +5,4 @@
 // Improve security in decentralized applications
 // Add automated smart contract testing
 // Enhance governance contract flexibility
+// Fix issues with cross-chain transaction finality
